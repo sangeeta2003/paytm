a0fefa490b2db938cc7e217dc836a2e5303422ed
*pknowledge* <mishrasangeeta944@gmail.com>
2024-02-27 20:55:04 +0530

    all schema

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,37 @@ const mongoose = require('mongoose')
  mongoose.connect("mongodb://localhost:27017/paytm")
 
  const userSchema = mongoose.Schema({
-    username:String,
-    password:String,
-    firstname:String,
-    lastname:String,
+    username:{
+     type: String,
+     required:true,
+     unique:true,
+     trim:true,
+     lowerCase:true,
+     maxlength:30,
+     minlength:5
+   },
+    password:{
+     type: String,
+     required:true,
+     unique:true,
+     maxlength:20,
+     minlength:5,
+
+   },
+    firstname:{
+      type:String,
+      required:true,
+      unique:true,
+      maxlength:20,
+      minlength:5,
+    },
+    lastname:{
+      type:String,
+      required:true,
+      unique:true,
+      maxlength:20,
+      minlength:5,
+    }
  })
 
  const User =mongoose.model("User",userSchema);
